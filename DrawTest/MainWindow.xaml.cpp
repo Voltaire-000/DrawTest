685a154836af0d7fda4251b2f74c24416b9823f6
Voltaire-000 <87976056+Voltare-000@users.noreply.github.com>
2023-03-24 13:10:27 +0700

    add code for draw line

--- DrawTest/MainWindow.xaml.cpp
+++ DrawTest/MainWindow.xaml.cpp
@@ -6,9 +6,15 @@
 #if __has_include("MainWindow.g.cpp")
 #include "MainWindow.g.cpp"
 #endif
+#include <winrt/Microsoft.UI.Xaml.Shapes.h>
+#include <winrt/Microsoft.UI.Xaml.Controls.h>
+#include <winrt/Microsoft.UI.Xaml.Media.h>
 
 using namespace winrt;
 using namespace Microsoft::UI::Xaml;
+using namespace Microsoft::UI::Xaml::Shapes;
+using namespace Microsoft::UI::Xaml::Controls;
+using namespace Microsoft::UI::Xaml::Media;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
@@ -34,8 +40,19 @@ namespace winrt::DrawTest::implementation
     {
         myButton().Content(box_value(L"Clicked"));
 
-        // Winui2D create drawing canvas
-
-        
+        // draw line using mx_line
+        mx_line().X1(0);
+        mx_line().Y1(0);
+        mx_line().X2(100);
+        mx_line().Y2(100);
+        mx_line().Stroke(Microsoft::UI::Xaml::Media::SolidColorBrush(Windows::UI::Colors::Red()));
+        mx_line().StrokeThickness(5);
+        mx_line().StrokeStartLineCap(Microsoft::UI::Xaml::Media::PenLineCap::Round);
+        mx_line().StrokeEndLineCap(Microsoft::UI::Xaml::Media::PenLineCap::Round);
+        mx_line().StrokeLineJoin(Microsoft::UI::Xaml::Media::PenLineJoin::Round);
+        mx_line().StrokeMiterLimit(1);
+        MainWindow::mx_canvas().Children().Append(mx_line());
+        MainWindow::mx_canvas().Children().Clear();
+               
     }
 }
