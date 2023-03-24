eebcaae07c00aaccc0ff351982fee645f53518bb
Voltaire-000 <87976056+Voltare-000@users.noreply.github.com>
2023-03-24 14:45:40 +0700

    in process

--- DrawTest/MainWindow.xaml.cpp
+++ DrawTest/MainWindow.xaml.cpp
@@ -23,9 +23,44 @@ using namespace Microsoft::UI::Xaml::Media;
 
 namespace winrt::DrawTest::implementation
 {
+    void DrawLine()
+    {
+
+        Microsoft::UI::Xaml::Controls::Canvas mx_canvas;
+        Microsoft::UI::Xaml::Shapes::Line mx_line;
+        mx_line.X1(0);
+        mx_line.Y1(0);
+        mx_line.X2(0); // set to 0 for now
+        mx_line.Y2(0); // set to 0 for now
+        mx_line.StrokeThickness(5);
+        mx_line.Stroke(Microsoft::UI::Xaml::Media::SolidColorBrush(Microsoft::UI::Colors::Aqua()));
+        mx_canvas.Children().Append(mx_line);
+
+        StackPanel stackPanel;
+        stackPanel.Children().Append(mx_canvas);
+
+
+        // add canvas to the visual tree
+        //Microsoft::UI::Xaml::Window window = Microsoft::UI::Xaml::Window::Current();
+        //window.Content(stackPanel);
+        //window.Content(mx_canvas);
+        //window.Activate();
+
+        //// wait for the layout pass to complete
+        //mx_canvas.UpdateLayout();
+
+        // set the line's end points based on the canvas size
+        mx_line.X2(mx_canvas.ActualWidth());
+        mx_line.Y2(mx_canvas.ActualHeight());
+    }
+
     MainWindow::MainWindow()
     {
+
+        DrawLine();
         InitializeComponent();
+
+        
     }
 
     int32_t MainWindow::MyProperty()
@@ -38,49 +73,10 @@ namespace winrt::DrawTest::implementation
         throw hresult_not_implemented();
     }
 
-    void DrawLine()
-    {
-        Microsoft::UI::Xaml::Controls::Canvas mx_canvas;
-        Microsoft::UI::Xaml::Shapes::Line mx_line;
-        mx_line.X1(0);
-        mx_line.Y1(0);
-        mx_line.X2(0); // set to 0 for now
-        mx_line.Y2(0); // set to 0 for now
-        mx_line.StrokeThickness(5);
-        mx_line.Stroke(Microsoft::UI::Xaml::Media::SolidColorBrush(Microsoft::UI::Colors::Aqua()));
-        mx_canvas.Children().Append(mx_line);
-
-        // add canvas to the visual tree
-        Microsoft::UI::Xaml::Window window = Microsoft::UI::Xaml::Window::Current();
-        window.Content(mx_canvas);
-        window.Activate();
-
-        // wait for the layout pass to complete
-        mx_canvas.UpdateLayout();
-
-        // set the line's end points based on the canvas size
-        mx_line.X2(mx_canvas.ActualWidth());
-        mx_line.Y2(mx_canvas.ActualHeight());
-    }
-
-
     void MainWindow::myButton_Click(IInspectable const&, RoutedEventArgs const&)
     {
         myButton().Content(box_value(L"Clicked"));
-
-        DrawLine();
-
-        //Microsoft::UI::Xaml::Controls::Canvas mx_canvas;
-        //Microsoft::UI::Xaml::Shapes::Line mx_line;
-
-        //mx_line.X1(0);
-        //mx_line.Y1(0);
-        //mx_line.X2(mx_canvas.ActualWidth());
-        //mx_line.Y2(mx_canvas.ActualHeight());
-        //mx_line.StrokeThickness(5);
-        //mx_line.Stroke(Microsoft::UI::Xaml::Media::SolidColorBrush(Microsoft::UI::Colors::Aqua()));
-        //mx_canvas.Children().Append(mx_line);
-              
+             
     }
 
 }
